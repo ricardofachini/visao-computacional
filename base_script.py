0eb2a6d6e81d904695b4d0e44edf9879727d87f8
Ricardo Fachini <ricardofachinisouza@gmail.com>
2024-11-28 21:10:32 -0300

    tensor flow version

--- base_script.py
+++ base_script.py
@@ -5,3 +5,7 @@ from keras.api.layers import Dense, Dropout, Activation, Flatten
 from keras.api.layers import Convolution2D, MaxPooling2D
 from keras.api.utils import to_categorical
 from keras.api.datasets import mnist
+
+import tensorflow
+
+print(tensorflow.__version__)
