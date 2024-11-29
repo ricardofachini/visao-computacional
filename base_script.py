88efb0dceaa53b10fdaad3cae30650abd4188163
Ricardo Fachini <ricardofachinisouza@gmail.com>
2024-11-28 21:09:34 -0300

    adds mask-r-cnn lib to project folder

---
+++ base_script.py
@@ -0,0 +1,7 @@
+# 3. Import libraries and modules
+import numpy as np
+from keras.api.models import Sequential
+from keras.api.layers import Dense, Dropout, Activation, Flatten
+from keras.api.layers import Convolution2D, MaxPooling2D
+from keras.api.utils import to_categorical
+from keras.api.datasets import mnist
